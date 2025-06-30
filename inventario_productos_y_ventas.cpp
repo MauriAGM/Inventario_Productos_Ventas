b78c1153b605f551947d2dfb1ea25d11108bf35d
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-29 22:16:17 -0500

    Avance 1 : Se agrega funcionalidad a las funciones void, ademas se implemento las variables constantes

--- inventario_productos_y_ventas.cpp
+++ inventario_productos_y_ventas.cpp
@@ -18,6 +18,8 @@ Requisitos:
 #include <iostream>
 using namespace std;
 
+const int MAX_P = 200;
+const int MAX_V = 100;
 struct Producto
 {
     string nombre;
@@ -31,29 +33,98 @@ struct Venta
     int cantidad;
     float precioTotal;
 };
+Producto productos[MAX_P];
+Venta ventas[MAX_V];
+int totalProductos = 0;
+int totalV = 0;
+int IdVENTA = 1;
 
 void RegistrarProducto (){
-    
+    cout << "Nombre del producto: ";
+    getline(cin,productos[totalProductos].nombre);
+    cout << "Precio del producto: ";
+    cin >> productos[totalProductos].precio;
+    totalProductos++;
 }
+
 void ListaProducto (){
-    
+    for (int i = 0; i < totalProductos; i++){
+        cout << productos[i].nombre << " -> " << productos[i].precio << endl;
+    }   
 }
 void BuscarProducto (){
+    string nombre;
+    cout << "Ingrese el nombre del producto: ";
+    getline(cin, nombre);
     
+    for (int i = 0; i < totalProductos; i++){
+        if (productos[i].nombre == nombre){
+            cout << "Producto encontrado: ";
+            cout << productos[i].nombre << " -> " << productos[i].precio << endl;
+        }
+    }
 }
 void ActualizarProducto (){
+    string nombre;
+    cout << "Ingrese el nombre del producto: ";
+    getline(cin, nombre);
+
+    for (int i = 0; i < totalProductos; i++){
+        if (productos[i].nombre == nombre){
+            cout << "Nuevo nombre de producto: ";
+            getline(cin,productos[i].nombre);
+            cout << "Nuevo precio de producto: ";
+            cin >> productos[i].precio;
+        }
+    }
 
 }
 void EliminarProducto (){
-    
+    string nombre;
+    cout << "Ingrese el nombre del producto: ";
+    getline(cin, nombre);
+
+    for (int i = 0; i < totalProductos; i++){
+        if (productos[i].nombre == nombre){
+            for (int j = i; j < totalProductos - 1; j++){
+                productos[j] = productos[j+1];
+            }
+            totalProductos--;
+            return;
+        }
+    }
 }
 void RegistrarVenta (){
-    
+    string nombre;
+    cout << "Ingrese el nombre del producto vendido: ";
+    getline(cin, nombre);
+    int cantidad;
+    cout << "Cantidad vendida: ";
+    cin >> cantidad;
+    for (int i = 0; i < totalProductos; i++){
+        if (productos[i].nombre == nombre){
+            ventas[totalV].IdVenta = IdVENTA++;
+            ventas[totalV].producto = nombre;
+            ventas[totalV].cantidad = cantidad;
+            ventas[totalV].precioTotal = productos[i].precio * cantidad;
+            totalV++;
+            return;
+        }
+    }
 }
 void ListaVenta (){
-    
+    for (int i = 0; i < totalV; i++){
+        cout << "Venta ID" << ventas[i].IdVenta;
+        cout << "Producto : " << ventas[i].producto;
+        cout << "Cantidad : " << ventas[i].cantidad;
+        cout << "Total: " << ventas[i].precioTotal << endl;
+    }
 }
 void TotalVenta (){
+    float total = 0;
+    for (int i = 0; i < totalV; i++){
+        total += ventas[i].precioTotal;
+    }
     
 }
 void menu (){
@@ -104,5 +175,6 @@ void menu (){
 }
 
 int main(){
-
+    menu();
+    return 0;
 }
